bb4ce0a0cfeaca9b0a1d8c0de513ec5957301aee
liiyung <y.liiyung@gmail.com>
2026-07-16 09:41:56 +0800

    added colour, fabric, modified 26-30, 21-26

--- wardrobe.js
+++ wardrobe.js
@@ -3,66 +3,92 @@ const wardrobe = {
         {
             name: "brandy melville white basic baby tee",
             path: "assets/tops/white_basic.png",
+            colour: ["white"],
+            fabric: ["cotton"],
             properties: ["breathable", "uncollared", "short sleeve", "not layerable"]
         },              
         {
             name: "pink uniqlo linen shirt",
             path: "assets/tops/pink_linen.png",
+            colour: ["pink"],
+            fabric: ["linen"],
             properties: ["breathable", "collared", "long sleeve", "layerable"]
         },
         {
             name: "pale blue sheer top",
             path: "assets/tops/paleblue_sheer.png",
-            properties: ["breathable", "uncollared", "short sleeve", "not layerable"]
+            colour: ["blue"],
+            fabric: ["cotton"],
+            properties: ["breathable", "uncollared", "short sleeve", "not layerable", "sheer"]
         },
         {
             name: "nagasaki striped top",
             path: "assets/tops/black-beige_striped.png",
+            colour: ["black", "beige"],
+            fabric: ["polyester"],
             properties: ["not breathable", "collared", "long sleeve", "layerable"]
         },
         {
             name: "wednesday white collar tartan top",
             path: "assets/tops/wednesday.png",
+            colour: ["navy", "green", "white", "tartan"],
+            fabric: ["cotton"],
             properties: ["breathable", "collared", "long sleeve", "layerable"]
         },
         {
             name: "pink red checkered top with lace trims",
             path: "assets/tops/pink_checkered.png",
+            colour: ["pink", "red", "checkered"],
+            fabric: ["cotton blend"],
             properties: ["breathable", "collared", "long sleeve", "layerable"]
         }, 
         {
             name: "grey short-sleeve shirt with heart cutout",
             path: "assets/tops/grey_heart.png",
+            colour: ["grey"],
+            fabric: ["cotton"],
             properties: ["breathable", "uncollared", "short sleeve", "not layerable"]
         }, 
         {
             name: "la perla black bustier",
             path: "assets/tops/black_bustier.png",
+            colour: ["black"],
+            fabric: ["polyamide"],
             properties: ["not breathable", "uncollared", "sleeveless", "not layerable"]
         },
         {
             name: "river island black cowl neck 3/4 sleeve",
             path: "assets/tops/black_cowl.png",
+            colour: ["black"],
+            fabric: ["?"],
             properties: ["not breathable", "uncollared", "long sleeve", "not layerable"]
         },  
         {
             name: "grey braless cami tank",
             path: "assets/tops/grey_braless.png",
+            colour: ["grey"],
+            fabric: ["polyester"],
             properties: ["breathable", "uncollared", "sleeveless", "not layerable"]
         },
         {
             name: "rubys brown green sheer shirt, reminds me of astronomy",
             path: "assets/tops/astronomer_green.png",
+            colour: ["green"],
+            fabric: ["nylon"],
             properties: ["not breathable", "collared", "short sleeve", "layerable"]
         },  
         {
             name: "black sbase",
             path: "assets/tops/black_sbase.png",
+            colour: ["black"],
+            fabric: ["cotton"],
             properties: ["breathable", "collared", "short sleeve", "layerable"]
         }, 
         {
             name: "butler navy shirt",
             path: "assets/tops/navy_butler.png",
+            colour: ["navy"],
+            fabric: ["acrylic"],
             properties: ["breathable", "collared", "long sleeve", "layerable"]
         },                                                                       
 
@@ -71,66 +97,92 @@ const wardrobe = {
         {
             name: "signature navy blue pinafore dress",
             path: "assets/bottoms/navy_signature.png",
+            colour: ["navy"],
+            fabric: ["cotton"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "beige pinafore dress copy",
             path: "assets/bottoms/beige_copy.png",
+            colour: ["beige"],
+            fabric: ["cotton"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "navy lolita dress",
             path: "assets/bottoms/navy_lolita.png",
+            colour: ["navy"],
+            fabric: ["polyester"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "novelty print venice skirt",
             path: "assets/bottoms/skirt_venice.png",
+            colour: ["green", "mint", "orange"],
+            fabric: ["polyester"],
             properties: ["not breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "kenzie kay black silk skirt",
             path: "assets/bottoms/black_silk.png",
+            colour: ["black"],
+            fabric: ["silk"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "cotton brown pants",
             path: "assets/bottoms/brown_pants.png",
+            colour: ["brown"],
+            fabric: ["cotton"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "black stretchy pinafore dress",
             path: "assets/bottoms/black_stretch.png",
+            colour: ["black"],
+            fabric: ["cotton"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "tartan secretary skirt",
             path: "assets/bottoms/skirt_tartan.png",
+            colour: ["navy", "green", "tartan"],
+            fabric: ["wool"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "black funeral pants",
             path: "assets/bottoms/black_funeral.png",
+            colour: ["black"],
+            fabric: ["cotton"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         },
         {
             name: "mermaid mini skirt",
             path: "assets/bottoms/skirt_mermaid_mini.png",
+            colour: ["black"],
+            fabric: ["polyester"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         }, 
         {
             name: "rubys navy wool longer mini skirt",
             path: "assets/bottoms/skirt_navy_wool.png",
+            colour: ["navy"],
+            fabric: ["wool"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         }, 
         {
             name: "brown wool shorts",
             path: "assets/bottoms/shorts_brown_wool.png",
+            colour: ["brown"],
+            fabric: ["wool"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         }, 
         {
             name: "lorraine skirt",
             path: "assets/bottoms/skirt_lorraine.png",
+            colour: ["black", "green", "pink"],
+            fabric: ["?"],
             properties: ["breathable", "uncollared", "sleeveless", "layerable"]
         }, 
     ]
